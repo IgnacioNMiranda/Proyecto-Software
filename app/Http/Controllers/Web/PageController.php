71863e13e01edab18bfcebbf3175db8e973e3f1d
Ignacio Miranda <ignacio.miranda01@alumnos.ucn.cl>
2020-03-02 16:17:53 -0600

    Casi despliegue completo de grupos de investigacion
    Despliegue casi terminado de grupos de investigacion en vista welcome

---
+++ app/Http/Controllers/Web/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\InvestigationGroup;
+
+class PageController extends Controller
+{
+    public function investigation_groups(){
+        $invGroups = InvestigationGroup::orderBy('id','DESC');
+
+        return view('welcome',compact('invGroups'));
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,62 +10,80 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
+
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <style>.dropdown:hover>.dropdown-menu {display: block;}</style>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="http://www.ucn.cl/">
+                <img src="https://www.ucn.cl/wp-content/uploads/2018/05/Escudo-UCN-Full-Color.png" width="50"
+                    height="50" alt="Logo UCN">
+            </a>
 
-                    </ul>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link text-white font-weight-bold btn-lg" href="{{ url('/') }}">Inicio <span
+                                class="sr-only">(current)</span></a>
+                    </li>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-white btn-lg" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Usuarios
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Crear usuario</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Editar usuario</a>
+                        </div>
+                    </li>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                </ul>
+
+                &nbsp;
+                <span class="navbar-text text-white btn-lg">Proyectos <span class="sr-only">(current)</span>
+                &nbsp;
+                <span class="navbar-text text-white btn-lg">Productos <span class="sr-only">(current)</span>
+
             </div>
+
+            @auth
+                <span class="navbar-text text-white d-inline">Bienvenido, {{ Auth::user()->name }} <span
+                    class="sr-only">(current)</span>
+            @endauth
+
+            @if (Route::has('login'))
+                <div class="top-right links navbar-item">
+                    @auth
+                    <a class="text-white navbar-text" href="{{ route('logout') }}" onclick="event.preventDefault();
+                          document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                        <span class="sr-only">(current)</span>
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                    @else
+                    <a href="{{ route('login') }}" class="text-white">Login <span class="sr-only">(current)</span></a>
+                    @endauth
+                </div>
+             @endif
+
         </nav>
 
         <main class="py-4">
@@ -72,4 +91,13 @@
         </main>
     </div>
 </body>
+
+<!--Archivos JS-->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<!-- Bootstrap Dropdown Hover JS -->
+<script src="js/bootstrap-dropdownhover.min.js"></script>
+
 </html>
