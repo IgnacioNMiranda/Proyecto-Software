cc923cf9778b3c59a3868cb7341d64bdc85a7e62
Ignacio Miranda PC <ignacio.miranda01@alumnos.ucn.cl>
2020-03-04 19:11:49 -0300

    Commit
    Commit sin cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,7 +109,7 @@
     </main>
 
     <footer class="page-footer font-small bg-primary pt-4">
-      
+
       <div class="container-fluid text-center text-md-left">
         <h4 class="text-white mb-3">Informacion adicional</h4>
         <div class="row">
@@ -129,7 +129,7 @@
   </div>
 
 
-  
+
   <!--Archivos JS-->
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
     integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
