1a86236fcee0880f4e7e647f6173c3641751818e
Fabián <fabian_antofa@hotmail.com>
2020-03-08 19:07:25 -0300

    crear producto v0.3, falta seleccion multiples investigadores

--- resources/views/admin-invest/products/partials/form.blade.php
+++ resources/views/admin-invest/products/partials/form.blade.php
@@ -4,29 +4,29 @@
             <div class = "panel panel default">
 
                 <div class="panel-body">
-                    <div class = "form-group">
-                        {{ Form::label('investigation_group_id', "Nombre del Grupo de investigacion")}}
-                        {{ Form::select('investigation_group_id', $invGroups, null, ['class' => 'form-control'])}}
-                    </div>
                     <div class="form-group">
                         {{ Form::label('name', 'Nombre del Producto') }}
                         {{ Form::text('name', null, ['class' => 'form-control', 'id' => 'name']) }}
                     </div>
+                    <div class = "form-group">
+                        {{ Form::label('investigation_group_id', "Nombre del Grupo de investigacion") }}
+                        {{ Form::select('investigation_group_id', $invGroups, null, ['placeholder' => 'Seleccione Grupo de investigacion...'], ['class' => 'form-control']) }}
+                    </div>
                     <div class="form-group">
                         {{ Form::label('description', 'Descripcion del Producto') }}
-                        {{ Form::text('description', null, ['class' => 'form-control', 'id' => 'name']) }}
+                        {{ Form::textarea('description', null, ['class' => 'form-control']) }}
                     </div>
                     <div class = "form-group">
-                        {{ Form::label('researcher', "Nombre de los Investigadores")}}
-                        {{ Form::select('researcher', $researchers, null, ['class' => 'form-control'])}}
+                        {{ Form::label('researcher', "Nombre de los Investigadores") }}
+                        {{ Form::select('researcher', $researchers, null, ['placeholder' => 'Seleccione Investigador(es)...'], ['class' => 'form-control']) }}
                     </div>
                     <div class = "form-group">
-                        {{ Form::label('Fecha', 'Fecha de Creacion') }}
-                        <input type="date" name="Fecha" disabled value="<?php echo date("Y-m-d");?>">
+                        {{ Form::label('date', 'Fecha de Creacion') }}
+                        <input type="date" name="date" disabled value="<?php echo date("Y-m-d");?>">  
                     </div>
                     <div class="form-group">
-                        {{ Form::label('project_id', "Nombre del Proyecto asociado")}}
-                        {{ Form::select('project_id', $projects, null, ['class' => 'form-control'])}}
+                        {{ Form::label('project_id', "Nombre del Proyecto asociado (opcional)") }}
+                        {{ Form::select('project_id', $projects, null, ['placeholder' => 'Seleccione Proyecto asociado...'], ['class' => 'form-control']) }}
                     </div>
                     <div class="form-group mt-4 d-flex justify-content-center">
                         {{ Form::submit('Crear Producto', ['class' => 'btn btn-secondary']) }}
