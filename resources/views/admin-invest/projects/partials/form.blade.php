3ddb42baa660a91ab1c0db39b185d12828198e00
Ignacio Miranda PC <ignacio.miranda01@alumnos.ucn.cl>
2020-03-09 02:15:51 -0300

    cambios menores Project

--- resources/views/admin-invest/projects/partials/form.blade.php
+++ resources/views/admin-invest/projects/partials/form.blade.php
@@ -3,20 +3,21 @@
     <div class="col-md-8 border shadow pt-4">
       <div class="panel panel default">
         <div class="panel-body">
-          <form action=" {{ route('projects.store')}}" method="POST">
+          <form action=" {{ route('projects.store') }}" method="POST">
+            {{ csrf_field() }}
 
             <div class="form-group">
-              <label for="investigation_groups">Grupo de Investigador Asociado</label>
-              <select class="form-control" id="investigation_groups">
-                <option selected>Seleccionar Grupo de Investigacion...</option>
+              <label for="investigation_group_id">Grupo de Investigador Asociado</label>
+              <select class="form-control" id="investigation_group_id">
+                <option disabled selected>Seleccionar Grupo de Investigación</option>
                 @foreach ($investigation_groups as $investigation_group)
-                <option value="{{ $investigation_group->id }}"> {{ $investigation_group->name }}</option>
+                  <option value="{{ $investigation_group->id }}"> {{ $investigation_group->name }}</option>
                 @endforeach
               </select>
             </div>
 
             <div class="form-group">
-              {{ Form::label('code', 'Codigo') }}
+              {{ Form::label('code', 'Código') }}
               {{ Form::text('code', null, ['class' => 'form-control', 'id' => 'code']) }}
             </div>
 
@@ -26,11 +27,11 @@
             </div>
 
             <div class="form-group">
-                <label class=" mr-sm-2" for="inlineFormCustomSelect">Estado</label>
-              <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                <option selected>Seleccionar Estado...</option>
+              <label class=" mr-sm-2" for="state">Estado</label>
+              <select class="custom-select mr-sm-2 form-control" id="state">
+                <option disabled selected>Seleccionar Estado</option>
                 <option value="1">Postulado</option>
-                <option value="2">En Ejecucion</option>
+                <option value="2">En Ejecución</option>
                 <option value="3">Finalizado</option>
                 <option value="4">Cancelado</option>
               </select>
@@ -41,23 +42,23 @@
               <select id="researchers" name="researchers[]" class="selectpicker" multiple data-live-search="true"
                 title="Seleccione Investigador(es) Asociado(s)">
                 @foreach ($researchers as $researcher)
-                <option value="{{ $researcher->id }}"> {{ $researcher->name }}</option>
+                  <option value="{{ $researcher->id }}"> {{ $researcher->name }}</option>
                 @endforeach
               </select>
             </div>
 
             <div class="form-group">
-              {{ Form::label('startDate', 'Fecha de Creacion') }}
-              <input type="date" name="startDate" id="startDate" disabled value="<?php echo date("Y-m-d");?>">
+              {{ Form::label('startDate', 'Fecha de Creación') }}
+              <input type="date" name="startDate" id="startDate" readonly value="<?php echo date("Y-m-d");?>">
             </div>
 
             <div class="form-group">
-              {{ Form::label('endDate', 'Fecha de Termino') }}
+              {{ Form::label('endDate', 'Fecha de Término') }}
               <input type="date" name="endDate" id="endDate" value="<?php echo date("Y-m-d");?>">
             </div>
 
             <div class="form-group mt-4 d-flex justify-content-center">
-              <button type="submit" class="btn btn-secondary mb-4" name="button">
+              <button type="submit" class="btn btn-secondary mb-4" name="projectSubmit">
                 {{ __('Guardar') }}
               </button>
             </div>
