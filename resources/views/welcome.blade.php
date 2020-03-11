746dce4e951d67ff00de3eb48dc647f911337f08
Ignacio Miranda PC <ignacio.miranda01@alumnos.ucn.cl>
2020-03-11 00:51:10 -0300

    Validación grupos de investigación inexistentes
    Ahora muestra un mensaje de que no existen grupos para mostrar cuando no hay grupos.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,32 +6,33 @@
     <p class="font-weight-bold text-dark pt-2 h5">Grupos de investigación</p>
 </div>
 
+@if ($invGroups->items() != null)
 
-<section class="container-fluid">
-    <div class="row" >
-
-        @foreach ($invGroups as $invGroup)
-        <div class="col-sm-12 col-md-6 col-lg-4 text-center" style="margin-bottom: 5rem;">
-            <a href="{{ route('investigationGroup', $invGroup->slug) }}" class="text-center">
-                @if ($invGroup->logo)
-                    <img class=" mx-auto rounded border" src="{{ asset($invGroup->logo) }}"
-                    alt="LogoGrupoInvestigacion" height="300" width="300">
-                @else
-                    <img class="img-fluid d-flex mx-auto" src="{{ asset('systemImages/signos_interrogacion.png') }}"
-                    width="300" height="300" alt="LogoGrupoInvestigacion">
-                @endif
-                <h3 class="text-dark d-flex d-inline justify-content-center">{{ $invGroup->name }}</h3>
-            </a>
+    <section class="container-fluid">
+        <div class="row">
+            @foreach ($invGroups as $invGroup)
+            <div class="col-sm-12 col-md-6 col-lg-4 text-center" style="margin-bottom: 5rem;">
+                <a href="{{ route('investigationGroup', $invGroup->slug) }}" class="text-center">
+                    @if ($invGroup->logo)
+                        <img class=" mx-auto rounded border" src="{{ asset($invGroup->logo) }}"
+                        alt="LogoGrupoInvestigacion" height="300" width="300">
+                    @else
+                        <img class="img-fluid d-flex mx-auto" src="{{ asset('systemImages/signos_interrogacion.png') }}"
+                        width="300" height="300" alt="LogoGrupoInvestigacion">
+                    @endif
+                    <h3 class="text-dark d-flex d-inline justify-content-center">{{ $invGroup->name }}</h3>
+                </a>
+            </div>
+            @endforeach
         </div>
-        @endforeach
+    </section>
 
-
-
+    <div style="margin-left:45%; margin-bottom:70px;">
+        {{ $invGroups->render() }}
     </div>
-</section>
 
-<div style="margin-left:45%; margin-bottom:70px;">
-    {{ $invGroups->render() }}
-</div>
+@else
+    <p class="display-4 text-center"> No existen grupos de investigación para mostrar </p>
+@endif
 
 @endsection
