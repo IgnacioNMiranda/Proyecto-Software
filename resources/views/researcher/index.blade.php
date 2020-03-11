ff69aaf534f7452774fee27f8880805893ff3d7e
Eduardo <edualvsald@hotmail.com>
2020-03-10 22:59:19 -0300

    Modificado Lista de Investigadores
    Se despliega Nombre, Pais y Unidad

--- resources/views/researcher/index.blade.php
+++ resources/views/researcher/index.blade.php
@@ -1,40 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
+@php
+use App\Unit;
+@endphp
+
 <div class="container mt-4 p-4">
     <div class="row justify-content-center">
-        <div class = "col-md-8 justify-content-center">   
+        <div class = "col-md-8 justify-content-center">
             <div class="panel panel-default">
                 <div class = "panel-heading h2 d-flex justify-content-center mb-4" >
                     Lista Investigadores
                 </div>
 
                 <div class="panel-body">
+
                     <table class="table table-striped table-hover">
                         <thead>
                             <tr>
-                                <th style="width: 3.5cm;">ID</th>
-                                <th>Nombre</th>
+                                <th style="width: 3.5cm;">Nombre</th>
+                                <th style="width: 3.5cm;">Pais</th>
+                                <th style="width: 3.5cm;">Unidad</th>
+
                                 <th colspan="2">&nbsp;</th>
 
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($researchers as $researcher)
+
+
                             <tr>
-                                <td> {{ $researcher->rut }} </td> 
-                                <td> {{ $researcher->researcher_name }} </td> 
+
+                                <td> {{ $researcher->researcher_name }} </td>
+                                <td> {{ $researcher->country }} </td>
+                                <td>@php
+                            $unit = Unit::find($researcher->unit_id);
+                            @endphp {{ $unit->name}} </td>
+
                                 <td width="10px">
-                                    <a href="#" class="btn btn-sm btn-default">
-                                        ver    
-                                    </a>    
+                                    <a href="{{ route('researchers.edit', $researcher->id) }}" class="btn btn-sm btn-secondary">
+                                        Editar
+                                    </a>
                                 </td>
-                                <td width="10px">
-                                    <a href="{{ route('researchers.edit', $researcher->id) }}" class="btn btn-sm btn-default">
-                                        editar    
-                                    </a>    
-                                </td>       
-                            </tr>    
+                            </tr>
                             @endforeach
                         </tbody>
                     </table>
