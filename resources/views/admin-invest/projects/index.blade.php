21336e0b1fb20c33428282d3c5b0e0a0e2560983
Ignacio <ignaciosantanderq12@hotmail.com>
2020-03-10 23:33:13 -0300

    Project View V.2.0.2

--- resources/views/admin-invest/projects/index.blade.php
+++ resources/views/admin-invest/projects/index.blade.php
@@ -4,44 +4,54 @@
 <div class="container mt-6 p-6">
     <div class="row justify-content-center">
         <div class="col-md-15 justify-content-center">
-            <div class="card-header">
-                Lista Proyectos
-            </div>
-            <div class="card-body">
-                <table class="table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th style="width: 1cm;">ID</th>
-                            <th style="width: 5cm;">Codigo</th>
-                            <th style="width: 10cm;">Nombre</th>
-                            <th style="width: 3cm;">Estado</th>
-                            <th style="width: 3cm;">Fecha de Inicio</th>
-                            <th style="width: 3cm;">Fecha de Finalización</th>
-                            <th style="width: 1cm;">ID Grupo de Investigación</th>
-                            <th colspan="3">&nbsp;</th>
+            <div class="card border-secondary">
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($projects as $project)
-                        <tr>
-                            <td> {{ $project->id }} </td>
-                            <td> {{ $project->code }} </td>
-                            <td> {{ $project->name }} </td>
-                            <td> {{ $project->state }} </td>
-                            <td> {{ $project->startDate }} </td>
-                            <td> {{ $project->endDate }} </td>
-                            <td> {{ $project->investigation_group_id }} </td>
-                            <td width="10px">
-                                <a href="{{ route('projects.edit', $project->id) }}" class="btn btn-sm btn-default">
-                                    editar
-                                </a>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                {{ $projects->render() }}
+                <div class ="card-header h2">
+                    Lista de Proyectos
+                    <a href="{{ route('projects.create') }}" class="btn btn-sm btn-success">Crear Proyecto</a>
+                </div>
+
+                <div class="card-body">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th style="width: 1cm;">ID</th>
+                                <th style="width: 5cm;">Codigo</th>
+                                <th style="width: 10cm;">Nombre</th>
+                                <th style="width: 3cm;">Estado</th>
+                                <th style="width: 3.5cm;">Fecha de Inicio</th>
+                                <th style="width: 3cm;">Fecha de Finalización</th>
+                                <th style="width: 1cm;">ID Grupo de Investigación</th>
+                                <th colspan="3">&nbsp;</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($projects as $project)
+                            <tr>
+                                <td> {{ $project->id }} </td>
+                                <td> {{ $project->code }} </td>
+                                <td> {{ $project->name }} </td>
+                                <td> {{ $project->state }} </td>
+                                <td> {{ $project->startDate }} </td>
+                                <td> {{ $project->endDate }} </td>
+                                <td> {{ $project->investigation_group_id }} </td>
+                                <td width="10px">
+                                    <a href="{{ route('projects.show', $project->id) }}" class="btn btn-sm btn-secondary">
+                                        Ver
+                                    </a>
+                                </td>
+                                <td width="10px">
+                                    <a href="{{ route('projects.edit', $project->id) }}" class="btn btn-sm btn-secondary">
+                                        Editar
+                                    </a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{ $projects->render() }}
+                </div>
             </div>
         </div>
     </div>
