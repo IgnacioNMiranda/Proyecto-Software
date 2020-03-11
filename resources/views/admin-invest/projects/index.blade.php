5eea9dd5f1880a2e83646e3e208bcbf339725273
Ignacio <ignaciosantanderq12@hotmail.com>
2020-03-10 22:11:49 -0300

    Project View V.2.0.1

--- resources/views/admin-invest/projects/index.blade.php
+++ resources/views/admin-invest/projects/index.blade.php
@@ -1,52 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-4 p-4">
+<div class="container mt-6 p-6">
     <div class="row justify-content-center">
-        <div class = "col-md-8 justify-content-center">   
-            <div class="panel panel-default">
-                <div class = "panel-heading h2 d-flex justify-content-center mb-4" >
-                    Lista Proyectos
-                </div>
-                <div class="panel-body">
-                    <table class="table table-striped table-hover">
-                        <thead>
-                            <tr>
-                                <th style="width: 3.5cm;">ID</th>
-                                <th>Codigo</th>
-                                <th>Nombre</th>
-                                <th>Estado</th>
-                                <th colspan="3">&nbsp;</th>
+        <div class="col-md-15 justify-content-center">
+            <div class="card-header">
+                Lista Proyectos
+            </div>
+            <div class="card-body">
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th style="width: 1cm;">ID</th>
+                            <th style="width: 5cm;">Codigo</th>
+                            <th style="width: 10cm;">Nombre</th>
+                            <th style="width: 3cm;">Estado</th>
+                            <th style="width: 3cm;">Fecha de Inicio</th>
+                            <th style="width: 3cm;">Fecha de Finalización</th>
+                            <th style="width: 1cm;">ID Grupo de Investigación</th>
+                            <th colspan="3">&nbsp;</th>
 
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($projects as $project)
-                            <tr>
-                                <td> {{ $project->id }} </td> 
-                                <td> {{ $project->code }} </td> 
-                                <td> {{ $project->name }} </td> 
-                                <td> {{ $project->state }} </td> 
-                                <td width="10px">
-                                    <a href="#" class="btn btn-sm btn-default">
-                                        ver    
-                                    </a>    
-                                </td>
-                                <td width="10px">
-                                    <a href="{{ route('projects.edit', $project->id) }}" class="btn btn-sm btn-default">
-                                        editar    
-                                    </a>    
-                                </td>       
-                            </tr>    
-                            @endforeach
-                        </tbody>
-                    </table>
-                    {{ $projects->render() }}
-                </div>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($projects as $project)
+                        <tr>
+                            <td> {{ $project->id }} </td>
+                            <td> {{ $project->code }} </td>
+                            <td> {{ $project->name }} </td>
+                            <td> {{ $project->state }} </td>
+                            <td> {{ $project->startDate }} </td>
+                            <td> {{ $project->endDate }} </td>
+                            <td> {{ $project->investigation_group_id }} </td>
+                            <td width="10px">
+                                <a href="{{ route('projects.edit', $project->id) }}" class="btn btn-sm btn-default">
+                                    editar
+                                </a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                {{ $projects->render() }}
             </div>
         </div>
     </div>
 </div>
+
 
 <div class="m-5 pb-5"></div>
 @endsection
