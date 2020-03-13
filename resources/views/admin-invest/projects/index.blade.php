03072331c40e1d2f72b69308f49e9b6a871fbe05
Ignacio <ignaciosantanderq12@hotmail.com>
2020-03-12 22:38:41 -0300

    Project View V.2.0.6

--- resources/views/admin-invest/projects/index.blade.php
+++ resources/views/admin-invest/projects/index.blade.php
@@ -16,12 +16,12 @@
                             <thead>
                                 <tr>
                                     <th style="width: 1cm;">ID</th>
-                                    <th style="width: 5cm;">Codigo</th>
+                                    {{-- <th style="width: 5cm;">Codigo</th> --}}
                                     <th style="width: 10cm;">Nombre</th>
                                     <th style="width: 3cm;">Estado</th>
                                     <th style="width: 3.8cm;">Fecha de Inicio</th>
                                     <th style="width: 3cm;">Fecha de Finalización</th>
-                                    <th style="width: 1cm;">ID Grupo de Investigación</th>
+                                    {{-- <th style="width: 1cm;">ID Grupo de Investigación</th> --}}
                                     <th colspan="3">&nbsp;</th>
 
                                 </tr>
@@ -30,12 +30,12 @@
                                 @foreach ($projects as $project)
                                 <tr>
                                     <td> {{ $project->id }} </td>
-                                    <td> {{ $project->code }} </td>
+                                    {{-- <td> {{ $project->code }} </td> --}}
                                     <td> {{ $project->name }} </td>
                                     <td> {{ $project->state }} </td>
                                     <td> {{ $project->startDate }} </td>
                                     <td> {{ $project->endDate }} </td>
-                                    <td> {{ $project->investigation_group_id }} </td>
+                                    {{-- <td> {{ $project->investigation_group_id }} </td> --}}
                                     <td width="10px">
                                         <a href="{{ route('projects.show', $project->id) }}" class="btn btn-sm btn-secondary">
                                             Ver
--- resources/views/admin-invest/projects/show.blade.php
+++ resources/views/admin-invest/projects/show.blade.php
@@ -1,17 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class = "container">
-    <div class = "row justify-content-center">
-        <div class = "col-md-8 border shadow pt-4">
-            <div class = "card border-secondary">
-                <div class = "card-header h4" >
-                    Ver Proyecto 
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8 border shadow pt-4">
+            <div class="card border-secondary">
+                <div class="card-header h4">
+                    Ver Proyecto: {{$project->name}}
                 </div>
-
                 <div class="card-body">
-                    <p><strong>Id</strong> {{$project->id}}</p>
-                    <p><strong>Nombre</strong> {{$project->id}}</p>
+                    <p><strong>Id:</strong> {{$project->id}}</p>
+                    <p><strong>Codigo:</strong> {{$project->code}}</p>
+                    <p><strong>Nombre:</strong> {{$project->name}}</p>
+                    <p><strong>Estado:</strong> {{$project->state}}</p>
+                    <p><strong>Fecha de Inicio:</strong> {{$project->startDate}}</p>
+                    <p><strong>Fecha de Finalización:</strong> {{$project->endDate}}</p>
+                    <p><strong>Id el Grupo de Investigación:</strong> {{$project->investigation_group_id}}</p>
+                    <div class="form-group mt-4 d-flex justify-content-center">
+                        <td width="10px">
+                            <a href="{{ route('projects.index', $project->id) }}" class="btn btn-sm btn-secondary">
+                                Volver
+                            </a>
+                        </td>
+                    </div>
+
+
                 </div>
 
             </div>
