fdd87e52adee9767f8746aa89356182fe8f148c5
Eduardo <edualvsald@hotmail.com>
2020-04-02 18:46:52 -0300

    testeo del edit

--- app/Http/Controllers/Publication/publicationController.php
+++ app/Http/Controllers/Publication/publicationController.php
@@ -35,10 +35,23 @@ class publicationController extends Controller
      */
     public function index(Request $request)
     {
-
         $publications = Publication::publicationType($request->get('publicationType'))
         ->orderBy('id','DESC')
         ->paginate();
+
+        if($request->get('researcher') != null){
+            $researcher_name = current(Researcher::where('researcher_name', $request->get('researcher'))->pluck('id')->all());
+            dd($researcher_name);
+
+            if(!$researcher_name){
+                $researcher_name= ' ';
+            }
+        }else{
+            $researcher_name = $request->get('researcher');
+        }
+
+
+
 
         return view('admin-invest.publications.index',compact('publications'));
     }
--- resources/views/admin-invest/publications/edit.blade.php
+++ resources/views/admin-invest/publications/edit.blade.php
@@ -210,32 +210,29 @@
 
 <script type="text/javascript">
     $(document).ready(function () {
-        function loadSubType(publicationResearcher){
+        function loadSubType(publicationSubType){
         var pubType = $('#pubType').val(); //Obtiene la id del grupo de investigacion
-        console.log(pubType)
+        console.log(publicationSubType);
+
             if(pubType=='Indexada'){
 
                 $('#pubSubType').html(" ");
                 $("#pubSubType").append('<option value="0" selected disabled>Seleccione SubTipo de la publicación</option>');
-                $("#pubSubType").append('<option value="1"  >WOS</option>');
-                $("#pubSubType").append('<option value="2"  >SCOPUS</option>');
-                $("#pubSubType").append('<option value="3"  >SCIELO</option>');
-                $("#pubSubType").append('<option value="4"  >Otro Indice</option>');
-                console.log(pubSubType)
-
+                $("#pubSubType").append('<option value="1" ' + (publicationSubType == "WOS" ? 'selected' : '') + '>WOS</option>');
+                $("#pubSubType").append('<option value="2" ' + (publicationSubType == "SCOPUS" ? 'selected' : '') + '>SCOPUS</option>');
+                $("#pubSubType").append('<option value="3" ' + (publicationSubType == "SCIELO" ? 'selected' : '') + '>SCIELO</option>');
+                $("#pubSubType").append('<option value="4" ' + (publicationSubType == "Otro Indice" ? 'selected' : '') + '>Otro Indice</option>');
 
             }else if(pubType=='No Indexada'){
                 $('#pubSubType').html(" ");
                 $("#pubSubType").append('<option value="0" selected disabled>Seleccione SubTipo de la publicación</option>');
-                $("#pubSubType").append('<option value="1"  >CONGRESO</option>');
-                $("#pubSubType").append('<option value="2"  >REVISTA</option>');
-                console.log(pubSubType)
+                $("#pubSubType").append('<option value="1" ' + (publicationSubType == "CONGRESO" ? 'selected' : '') + '>CONGRESO</option>');
+                $("#pubSubType").append('<option value="2" ' + (publicationSubType == "REVISTA" ? 'selected' : '') + '>REVISTA</option>');
 
             }
         }
-        var publicationResearcher = {!! json_encode($publication->researcher) !!};
-
-        loadSubType(publicationResearcher);
+        var publicationSubType = {!! json_encode($publication->publicationSubType) !!};
+        loadSubType(publicationSubType);
         $(document).on('change', '#pubType', loadSubType);
     });
 </script>
